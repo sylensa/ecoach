65232c4abaeeb822925e90f01df7eb882d5b74aa
Samuel Quaye <samcpu7@gmail.com>
2021-08-21 18:35:04 +0100

    implement login and signup

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -17,10 +17,11 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
-  String name = "";
+  String firstName = "";
+  String lastName = "";
   String email = "";
+  String phone = "";
   String password = "";
-  List departments = [];
   bool isLoading = false;
 
   doRegister(BuildContext context) async {
@@ -32,8 +33,15 @@ class _RegisterPageState extends State<RegisterPage> {
     http.Response response = await http.post(
       Uri.parse(AppUrl.register),
       headers: {'Content-Type': 'application/json'},
-      body: json
-          .encode(<String, dynamic>{'identifier': email, "password": password}),
+      body: json.encode(<String, dynamic>{
+        'fname': firstName,
+        'lname': lastName,
+        'gender': _gender,
+        'email': email,
+        'phone': phone,
+        "password": password,
+        "password_confirmed": password
+      }),
     );
 
     if (response.statusCode == 200) {
@@ -78,8 +86,8 @@ class _RegisterPageState extends State<RegisterPage> {
   String? _selectedGender = 'Gender';
   List<String> _gender = ['Gender', 'Male', 'Female'];
 
-  String? _selectedAge = "Age";
-  List<String> _ages = ['Age', 'Adult', 'Youth', 'Teen', 'Children'];
+  String? selectedUserType = "student";
+  List<String> userType = ['student', 'teacher', 'parent'];
 
   setLoggedInUser(User user) async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -109,14 +117,30 @@ class _RegisterPageState extends State<RegisterPage> {
                       height: 70,
                     ),
                     TextFormField(
-                      decoration: InputDecoration(labelText: 'Full Name'),
+                      decoration: InputDecoration(labelText: 'First Name'),
                       onSaved: (value) {
-                        name = value!;
+                        firstName = value!;
                       },
                       validator: (text) {
                         String _msg = "";
                         if (text!.isEmpty) {
-                          _msg = "Your name is required";
+                          _msg = "Your first name is required";
+                        }
+                        return _msg;
+                      },
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Last Name'),
+                      onSaved: (value) {
+                        lastName = value!;
+                      },
+                      validator: (text) {
+                        String _msg = "";
+                        if (text!.isEmpty) {
+                          _msg = "Your last name is required";
                         }
                         return _msg;
                       },
@@ -137,6 +161,26 @@ class _RegisterPageState extends State<RegisterPage> {
                           _msg = "Your email is required";
                         } else if (!regex.hasMatch(text)) {
                           _msg = "Please provide a valid email address";
+                        }
+                        return _msg;
+                      },
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Phone'),
+                      onSaved: (value) {
+                        phone = value!;
+                      },
+                      validator: (text) {
+                        String _msg = "";
+                        RegExp regex = new RegExp(
+                            r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
+                        if (text!.isEmpty) {
+                          _msg = "Your phone number is required";
+                        } else if (!regex.hasMatch(text)) {
+                          _msg = "Please provide a valid phone";
                         }
                         return _msg;
                       },
@@ -169,17 +213,17 @@ class _RegisterPageState extends State<RegisterPage> {
                     ),
                     DropdownButton(
                         isExpanded: true,
-                        value: _selectedAge,
-                        items: _ages.map((String val) {
+                        value: selectedUserType,
+                        items: userType.map((String val) {
                           return new DropdownMenuItem<String>(
                             value: val,
                             child: new Text(val),
                           );
                         }).toList(),
-                        hint: Text("Age"),
+                        hint: Text("User Type"),
                         onChanged: (String? newVal) {
                           setState(() {
-                            _selectedAge = newVal;
+                            selectedUserType = newVal;
                           });
                         }),
                     SizedBox(
@@ -213,10 +257,9 @@ class _RegisterPageState extends State<RegisterPage> {
 
                         if (text!.isEmpty) {
                           _msg = "Your password is required";
+                        } else if (text != password) {
+                          return "Password must be same as above";
                         }
-                        // else if (text != password) {
-                        //   return "Password must be same as above";
-                        // }
                         return _msg;
                       },
                     ),
