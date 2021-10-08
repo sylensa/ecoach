2902f81417863636de2fadfa66f5ed0cc9a1902f
Samuel Quaye <samcpu7@gmail.com>
2021-10-08 21:14:14 +0000

    minor fixes

--- lib/views/store.dart
+++ lib/views/store.dart
@@ -67,167 +67,144 @@ class _StorePageState extends State<StorePage> {
     final orientation = MediaQuery.of(context).orientation;
 
     return SafeArea(
-      child: Scaffold(
-        backgroundColor: Color(0xFF28BFDF),
-        body: Column(
-          children: [
-            SizedBox(
-              height: 40,
+      child: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          backgroundColor: Color(0xFF28BFDF),
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.directions_transit)),
+              ],
             ),
-            SizedBox(
-              width: double.infinity,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ToggleButtons(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text("Monthly"),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text("Annually"),
-                      )
-                    ],
-                    isSelected: _isSelected,
-                    onPressed: (int index) {
-                      setState(() {
-                        for (int i = 0; i < _isSelected.length; i++) {
-                          _isSelected[i] = i == index;
-                        }
-                      });
-                    },
-                    color: Colors.black,
-                    selectedColor: Colors.orange.shade100,
-                    fillColor: Colors.blue.shade600,
-                    borderColor: Colors.blue.shade700,
-                    selectedBorderColor: Colors.red,
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                  ),
-                ],
-              ),
-            ),
-            FutureBuilder(
-              future: futurePlans,
-              builder: (context, snapshot) {
-                print(snapshot.data);
-                switch (snapshot.connectionState) {
-                  case ConnectionState.none:
-                  case ConnectionState.waiting:
-                    return Center(child: CircularProgressIndicator());
-                  default:
-                    if (snapshot.hasError)
-                      return Text('Error: ${snapshot.error}');
-                    else if (snapshot.data != null) {
-                      ApiResponse response = snapshot.data! as ApiResponse;
+          ),
+          body: TabBarView(
+            children: [
+              FutureBuilder(
+                future: futurePlans,
+                builder: (context, snapshot) {
+                  print(snapshot.data);
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.none:
+                    case ConnectionState.waiting:
+                      return Center(child: CircularProgressIndicator());
+                    default:
+                      if (snapshot.hasError)
+                        return Text('Error: ${snapshot.error}');
+                      else if (snapshot.data != null) {
+                        ApiResponse response = snapshot.data! as ApiResponse;
 
-                      List<Plan>? plans = response.data as List<Plan>;
-                      print(plans);
-                      return SingleChildScrollView(
-                        child: Container(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Padding(
-                                  padding: const EdgeInsets.all(12.0),
-                                  child: Container(
-                                    child: plans.length > 0
-                                        ? Container(
-                                            child: ListView.builder(
-                                                shrinkWrap: true,
-                                                itemCount: plans.length,
-                                                itemBuilder: (context, index) {
-                                                  return Padding(
-                                                    padding: const EdgeInsets
-                                                            .fromLTRB(
-                                                        20, 12, 20, 12),
-                                                    child: InkWell(
-                                                      onTap: () {
-                                                        Navigator.push(context,
-                                                            MaterialPageRoute(
-                                                                builder:
-                                                                    (context) {
-                                                          return SubscribePage(
-                                                              widget.user,
-                                                              plans[index]);
-                                                        }));
-                                                      },
-                                                      highlightColor:
-                                                          Colors.blue.shade600,
-                                                      child: Container(
-                                                        // height: 80,
-                                                        decoration: BoxDecoration(
-                                                            border: Border.all(
-                                                                width: 1,
-                                                                color: Colors
-                                                                    .white),
-                                                            borderRadius:
-                                                                BorderRadius
-                                                                    .circular(
-                                                                        10)),
-                                                        child: Padding(
-                                                          padding:
-                                                              const EdgeInsets
-                                                                  .all(14.0),
-                                                          child: Row(
-                                                            children: [
-                                                              Text(
-                                                                  plans[index]
-                                                                      .name!,
-                                                                  style: TextStyle(
-                                                                      color: Colors
-                                                                          .white,
-                                                                      fontSize:
-                                                                          18,
-                                                                      fontWeight:
-                                                                          FontWeight
-                                                                              .bold)),
-                                                              Spacer(),
-                                                              Text(
-                                                                  money(plans[
-                                                                          index]
-                                                                      .price!),
-                                                                  style: TextStyle(
-                                                                      color: Colors
-                                                                          .white,
-                                                                      fontSize:
-                                                                          18)),
-                                                            ],
+                        List<Plan>? plans = response.data as List<Plan>;
+                        print(plans);
+                        return SingleChildScrollView(
+                          child: Container(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Padding(
+                                    padding: const EdgeInsets.all(12.0),
+                                    child: Container(
+                                      child: plans.length > 0
+                                          ? Container(
+                                              child: ListView.builder(
+                                                  shrinkWrap: true,
+                                                  itemCount: plans.length,
+                                                  itemBuilder:
+                                                      (context, index) {
+                                                    return Padding(
+                                                      padding: const EdgeInsets
+                                                              .fromLTRB(
+                                                          20, 12, 20, 12),
+                                                      child: InkWell(
+                                                        onTap: () {
+                                                          Navigator.push(
+                                                              context,
+                                                              MaterialPageRoute(
+                                                                  builder:
+                                                                      (context) {
+                                                            return SubscribePage(
+                                                                widget.user,
+                                                                plans[index]);
+                                                          }));
+                                                        },
+                                                        highlightColor: Colors
+                                                            .blue.shade600,
+                                                        child: Container(
+                                                          // height: 80,
+                                                          decoration: BoxDecoration(
+                                                              border: Border.all(
+                                                                  width: 1,
+                                                                  color: Colors
+                                                                      .white),
+                                                              borderRadius:
+                                                                  BorderRadius
+                                                                      .circular(
+                                                                          10)),
+                                                          child: Padding(
+                                                            padding:
+                                                                const EdgeInsets
+                                                                    .all(14.0),
+                                                            child: Row(
+                                                              children: [
+                                                                Text(
+                                                                    plans[
+                                                                            index]
+                                                                        .name!,
+                                                                    style: TextStyle(
+                                                                        color: Colors
+                                                                            .white,
+                                                                        fontSize:
+                                                                            18,
+                                                                        fontWeight:
+                                                                            FontWeight.bold)),
+                                                                Spacer(),
+                                                                Text(
+                                                                    money(plans[
+                                                                            index]
+                                                                        .price!),
+                                                                    style: TextStyle(
+                                                                        color: Colors
+                                                                            .white,
+                                                                        fontSize:
+                                                                            18)),
+                                                              ],
+                                                            ),
                                                           ),
                                                         ),
                                                       ),
-                                                    ),
-                                                  );
-                                                }),
-                                          )
-                                        : Container(
-                                            child: SizedBox(
-                                              height: 400,
-                                              child: Center(
-                                                child: Text(
-                                                    "There are no items in the store"),
+                                                    );
+                                                  }),
+                                            )
+                                          : Container(
+                                              child: SizedBox(
+                                                height: 400,
+                                                child: Center(
+                                                  child: Text(
+                                                      "There are no items in the store"),
+                                                ),
                                               ),
                                             ),
-                                          ),
-                                  )),
-                            ],
+                                    )),
+                              ],
+                            ),
                           ),
-                        ),
-                      );
-                    } else if (snapshot.data == null)
-                      return Container(
-                          child: Center(
-                        child: Text("There are no items in the store"),
-                      ));
-                    else
-                      return Container();
-                }
-              },
-            ),
-          ],
+                        );
+                      } else if (snapshot.data == null)
+                        return Container(
+                            child: Center(
+                          child: Text("There are no items in the store"),
+                        ));
+                      else
+                        return Container();
+                  }
+                },
+              ),
+              Icon(Icons.directions_transit),
+            ],
+          ),
         ),
       ),
     );
