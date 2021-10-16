8171ad945302c549de77425249226e7037aae415
Samuel Quaye <samcpu7@gmail.com>
2021-10-16 20:14:10 +0000

    implementing download again

---
+++ lib/api/file_downloader.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:ecoach/utils/shared_preference.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' show join;
+
+class FileDownloader {
+  final String url;
+  String filename;
+
+  FileDownloader(this.url, {required this.filename});
+
+  downloadFile(Function(int percentage) publishSubject) async {
+    Dio dio = Dio();
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, filename);
+
+    String? token = await UserPreferences().getUserToken();
+
+    return await dio.download(url, path,
+        options: Options(
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'api-token': token ?? ""
+          },
+        ), onReceiveProgress: (received, total) {
+      int percentage = ((received / total) * 100).floor();
+      publishSubject(percentage);
+    });
+  }
+}
