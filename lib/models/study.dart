adc5a45d7033eb68837b44e3bec5ae88edec1667
Samuel Quaye <samcpu7@gmail.com>
2021-11-01 14:39:42 +0000

    implement learn mode screen

---
+++ lib/models/study.dart
@@ -0,0 +1,16 @@
+class Study {
+  int? id;
+  String? type;
+  String? name;
+  int? topicId;
+  int? courseId;
+  int? userId;
+
+  Study(
+      {this.id,
+      this.type,
+      this.name,
+      this.topicId,
+      this.courseId,
+      this.userId});
+}
