a32a8450bd21123bd35338960f0379244fc177ab
Samuel Quaye <samcpu7@gmail.com>
2022-05-27 11:47:04 +0000

    google signin

---
+++ lib/api/google_signin_call.dart
@@ -0,0 +1,26 @@
+import 'package:google_sign_in/google_sign_in.dart';
+
+class GoogleSignInApi {
+  GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  Future<void> signIn(Function(String? idToken) callback) async {
+    try {
+      await _googleSignIn.signIn().then((result) {
+        result!.authentication.then((googleKey) async {
+          // print(googleKey.accessToken);
+          print(googleKey.idToken);
+          // print(_googleSignIn.currentUser!.displayName);
+          callback(googleKey.idToken);
+        }).catchError((err) {
+          print('inner error');
+        });
+      }).catchError((err, m) {
+        print('error occured');
+        print(err);
+        print(m);
+      });
+    } catch (error) {
+      print(error);
+    }
+  }
+}
