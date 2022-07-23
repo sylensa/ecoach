e22684b06ffab92e8482704c006c4f50e0e91cda
Sylensa <sylensa.adolf@gmail.com>
2022-07-23 02:23:43 +0000

    UI modification

--- lib/views/group/group_page.dart
+++ lib/views/group/group_page.dart
@@ -8,6 +8,7 @@ import 'package:ecoach/utils/constants.dart';
 import 'package:ecoach/utils/style_sheet.dart';
 import 'package:ecoach/views/commission/commission_agent_page.dart';
 import 'package:ecoach/views/group/group_list.dart';
+import 'package:ecoach/views/group/test_creation/test_creation.dart';
 import 'package:ecoach/widgets/toast.dart';
 import 'package:ecoach/widgets/widgets.dart';
 import 'package:flutter/material.dart';
@@ -399,7 +400,6 @@ class _GroupPageState extends State<GroupPage> {
     });
 
   }
-
   makeUserAdmin(String userId) async {
     final bool isConnected = await InternetConnectionChecker().hasConnection;
     if(isConnected){
@@ -433,7 +433,133 @@ class _GroupPageState extends State<GroupPage> {
     }
   }
 
+  announcementModalBottomSheet(context,){
+    TextEditingController titleController = TextEditingController();
+    TextEditingController descriptionController = TextEditingController();
+    bool isActivated = true;
+    double sheetHeight = 600;
+    showModalBottomSheet(
+        context: context,
+        isDismissible: true,
+        backgroundColor: Colors.transparent ,
+        isScrollControlled: true,
+        builder: (BuildContext context){
+          return StatefulBuilder(
+            builder: (BuildContext context,StateSetter stateSetter){
+              return Container(
+                  height: sheetHeight,
+                  decoration: BoxDecoration(
+                      color: kAdeoGray ,
+                      borderRadius: BorderRadius.only(topLeft: Radius.circular(30),topRight: Radius.circular(30),)
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 20,),
+                      Container(
+                        color: Colors.grey,
+                        height: 5,
+                        width: 100,
+                      ),
+                      SizedBox(height: 20,),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: Center(child: sText("Announcement",weight: FontWeight.bold,size: 20,align: TextAlign.center)),
+                      ),
+                      SizedBox(height: 10,),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: sText("Select Your Preferred Upgrade",color: kAdeoGray3,weight: FontWeight.w400,align: TextAlign.center),
+                      ),
+                      SizedBox(height: 20,),
+                      Expanded(
+                        child: ListView(
+                          children: [
+                            Container(
+                              padding: EdgeInsets.only(left: 20,right: 20),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Expanded(
+                                    child: TextFormField(
+                                      // autofocus: true,
+                                      controller:titleController,
+                                      validator: (value) {
+                                        if (value == null || value.isEmpty) {
+                                          return 'Please check that you\'ve entered group email';
+                                        }
+                                        return null;
+                                      },
+                                      decoration: textDecorNoBorder(
+                                        radius: 10,
+                                        labelText: "Name",
+                                        hintColor: Color(0xFFB9B9B9),
+                                        borderColor:Colors.white ,
+                                        fill: Colors.white,
+                                        padding: EdgeInsets.only(left: 10,right: 10),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(height: 20,),
+                            Container(
+                              padding: EdgeInsets.only(left: 20,right: 20),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Expanded(
+                                    child: TextFormField(
+                                      // autofocus: true,
+                                      controller: descriptionController,
+                                      validator: (value) {
+                                        if (value == null || value.isEmpty) {
+                                          return 'Please check that you\'ve entered your group description';
+                                        }
+                                        return null;
+                                      },
+                                      maxLines: 10,
+                                      decoration: textDecorNoBorder(
+                                        radius: 10,
+                                        labelText: "Description",
+                                        hintColor: Color(0xFFB9B9B9),
+                                        borderColor:Colors.white ,
+                                        fill: Colors.white,
+                                        padding: EdgeInsets.only(left: 10,right: 10),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(height: 20,),
+                            Container(
+                              child: Image.asset("assets/images/upload_images.png")
+                            )
+                          ],
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: (){
+                          showLoaderDialog(context);
+                        },
+                        child: Container(
+                          width: appWidth(context),
+                          color: Color(0xFF00C9B9),
+                          padding: EdgeInsets.symmetric(horizontal: 30,vertical: 20),
+                          child: sText("Send Announcement",color: Colors.white,weight: FontWeight.w500,align: TextAlign.center,size: 18),
+                        ),
+                      ),
 
+                    ],
+                  )
+              );
+            },
+
+          );
+        }
+    );
+  }
 
 
   @override
@@ -755,55 +881,74 @@ class _GroupPageState extends State<GroupPage> {
                             leading: Container(
                               child: sText("Announcements",weight: FontWeight.w500,size: 16),
                             ),
+
                             trailing:  Container(
                               child: Icon(Icons.add_circle_outline,color: Colors.black,),
                             ),
+
                             title: Container()  ,
                             children: <Widget>[
+                            GestureDetector(
+                              onTap: (){
+                                announcementModalBottomSheet(context);
+                              },
+                              child: Container(
+                                child:  sText("New Announcements",weight: FontWeight.w500,size: 16,align: TextAlign.center),
+                                padding: EdgeInsets.symmetric(vertical: 15,horizontal: 30),
+                                decoration: BoxDecoration(
+                                  color: Color(0XFFF0F7FF),
+                                  border: Border.all(color: Color(0XFF489CFF)),
+                                  borderRadius: BorderRadius.circular(10)
+                                ),
+                              ),
+                            ),
+                                SizedBox(height: 20,),
                               if(listGroupViewData[0].admins!.isNotEmpty)
-                                Container(
-                                  padding: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
-                                  margin: EdgeInsets.symmetric(horizontal: 20),
-                                  decoration: BoxDecoration(
-                                      color: Colors.white,
-                                      borderRadius: BorderRadius.circular(5)
-                                  ),
-                                  child: Column(
-                                    children: [
-                                      for(int i = 0; i< listGroupViewData[0].admins!.length; i++)
-                                        Column(
-                                          children: [
-                                            Row(
+                                Column(
+                                  children: [
+                                    for(int i = 0; i< 2; i++)
+
+                                      Column(
+                                        children: [
+                                          Container(
+                                            decoration: BoxDecoration(
+                                                color: Colors.white,
+                                                borderRadius: BorderRadius.circular(5)
+                                            ),
+                                            margin: EdgeInsets.symmetric(horizontal: 20),
+                                            padding: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+
+                                            child: Column(
+                                              crossAxisAlignment: CrossAxisAlignment.start,
                                               children: [
-                                                Stack(
+                                                Row(
                                                   children: [
-                                                    displayLocalImage("filePath",radius: 30),
-                                                    Positioned(
-                                                      bottom: 5,
-                                                      right: 0,
-                                                      child: Image.asset("assets/images/tick-mark.png"),
-                                                    )
+                                                   sText("Victor Adatsi",color: Colors.black,weight: FontWeight.bold),
+                                                   sText(" (group admin)",color: kAdeoGray2),
+                                                    Expanded(child: Container()),
+                                                    Icon(Icons.arrow_forward_ios,color: kAdeoGray3,size: 16,)
                                                   ],
                                                 ),
-                                                SizedBox(width: 10,),
-                                                Column(
-                                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                                  children: [
-                                                    sText(listGroupViewData[0].admins![i].name,color: Colors.black,weight: FontWeight.w500),
-                                                    SizedBox(height: 5,),
-                                                    sText("Admin",color: kAdeoGray3,size: 12),
-                                                  ],
+                                                SizedBox(height: 10,),
+                                                sText("10 mins ago",color: kAdeoGray2),
+                                                SizedBox(height: 10,),
+                                                Container(
+                                                  child:   sText("Read chapters 9 to 10 of the textbook before class tomorrow",color: kAdeoGray3,weight: FontWeight.w500),
                                                 ),
-                                                Expanded(child: Container()),
-                                                Icon(Icons.arrow_forward_ios,color: kAdeoGray3,size: 16,)
+                                                SizedBox(height: 10,),
+                                                Container(
+                                                  child: Image.asset("assets/images/First Screen.png"),
+                                                )
                                               ],
                                             ),
-                                            SizedBox(height: 10,)
-                                          ],
-                                        ),
+                                          ),
+                                          listMembers.length -1 != i ?
+                                          SizedBox(height: 10,child: Container(color: kHomeBackgroundColor,),): Container(),
+                                        ],
+                                      ),
 
-                                    ],
-                                  ),
+
+                                  ],
                                 ),
                             ],
                           ),
@@ -832,6 +977,21 @@ class _GroupPageState extends State<GroupPage> {
                             ),
                             title: Container()  ,
                             children: <Widget>[
+                              GestureDetector(
+                                onTap: (){
+                                 goTo(context, TestCreation());
+                                },
+                                child: Container(
+                                  child:  sText("New Test",weight: FontWeight.w500,size: 16,align: TextAlign.center),
+                                  padding: EdgeInsets.symmetric(vertical: 15,horizontal: 30),
+                                  decoration: BoxDecoration(
+                                      color: Color(0XFFF0F7FF),
+                                      border: Border.all(color: Color(0XFF489CFF)),
+                                      borderRadius: BorderRadius.circular(10)
+                                  ),
+                                ),
+                              ),
+                              SizedBox(height: 20,),
                               if(listGroupViewData[0].admins!.isNotEmpty)
                                 Container(
                                   padding: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
