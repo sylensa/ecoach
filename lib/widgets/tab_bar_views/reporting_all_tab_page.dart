f0be6dd99a8ff8feeb23d4d0bedbe27eea7cf277
Sylensa <sylensa.adolf@gmail.com>
2022-07-26 21:31:38 +0000

    analysis graph added

--- lib/widgets/tab_bar_views/reporting_all_tab_page.dart
+++ lib/widgets/tab_bar_views/reporting_all_tab_page.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:ecoach/controllers/average_score_graph.dart';
 import 'package:ecoach/controllers/test_controller.dart';
 import 'package:ecoach/database/questions_db.dart';
 import 'package:ecoach/database/test_taken_db.dart';
@@ -18,6 +19,7 @@ import 'package:ecoach/views/results_ui.dart';
 import 'package:ecoach/widgets/buttons/adeo_text_button.dart';
 import 'package:ecoach/widgets/percentage_switch.dart';
 import 'package:ecoach/widgets/widgets.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class AllTabPage extends StatefulWidget {
@@ -37,6 +39,71 @@ class _AllTabPageState extends State<AllTabPage> {
   late bool showInPercentage;
   dynamic selected = null;
   Future<List<TestTaken>>? tests;
+  List <TestTaken> graphTestData = [];
+  String dropdownValue = 'All';
+  List<FlSpot> graphData = [];
+  getStats(String period)async{
+    graphTestData.clear();
+    graphData.clear();
+    graphTestData = await TestTakenDB().courseTestsTakenPeriod(widget.course.id!,period);
+    for (int i = 0; i < graphTestData.length; i++) {
+      final test = graphTestData[i];
+      graphData.add(
+        FlSpot(
+          (i + 1).toDouble(),
+          double.parse(test.score!.toStringAsFixed(2)),
+        ),
+      );
+    }
+    setState((){
+
+    });
+  }
+  buildDropDownButton() {
+    return DropdownButton<String>(
+      dropdownColor: Colors.blue,
+      value: dropdownValue,
+      icon: const Icon(
+        Icons.arrow_drop_down,
+        color: Colors.white,
+      ),
+      iconSize: 24,
+      elevation: 16,
+      style: const TextStyle(
+        color: Colors.white,
+      ),
+      underline: Container(
+        height: 0,
+        color: Colors.black,
+      ),
+      onChanged: (String? newValue) {
+        setState(() {
+          dropdownValue = newValue!;
+          getStats(dropdownValue);
+          print(dropdownValue);
+        });
+      },
+      items: <String>[
+        'All',
+        'Daily',
+        'Weekly',
+        'Monthly',
+      ].map<DropdownMenuItem<String>>(
+            (String value) {
+          return DropdownMenuItem<String>(
+
+            value: value,
+            child: Text(
+              value,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          );
+        },
+      ).toList(),
+    );
+  }
 
   handleSelection(test) {
     setState(() {
@@ -73,6 +140,7 @@ class _AllTabPageState extends State<AllTabPage> {
   @override
   void initState() {
     super.initState();
+    getStats('All');
     showInPercentage = false;
     tests = TestTakenDB().courseTestsTaken(widget.course.id!);
   }
@@ -86,6 +154,129 @@ class _AllTabPageState extends State<AllTabPage> {
   Widget build(BuildContext context) {
     return Column(
       children: [
+        //graph
+        Padding(
+          padding: EdgeInsets.only(
+            left: MediaQuery.of(context).size.width * 0.05,
+            right: MediaQuery.of(context).size.width * 0.05,
+            top: MediaQuery.of(context).size.height * 0.02,
+          ),
+          child: Card(
+            elevation: 0.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5),
+            ),
+            color: Colors.blue.withOpacity(0.05),
+            child: ExpansionTile(
+              collapsedTextColor: Colors.black,
+              collapsedIconColor: Colors.black,
+              iconColor: Colors.black,
+              title: const Text(
+                'Performance Graph',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.blue,
+                ),
+              ),
+              children: [
+                Stack(
+                  fit: StackFit.passthrough,
+                  children: [
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          top: 5,
+                          right: 12,
+                        ),
+                        child: Container(
+                          height: 30,
+                          width: 80,
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: Center(
+                            child: buildDropDownButton(),
+                          ),
+                        ),
+                      ),
+                    ),
+                    // sfCartesianChart(),
+                    Container(
+                      padding: const EdgeInsets.only(
+                        right: 18.0,
+                        left: 12.0,
+                        top: 24,
+                        bottom: 12,
+                      ),
+                      height: 250,
+                      child: LineChart(
+                        LineChartData(
+                          minX: 1,
+                          maxY: 100.0,
+                          minY: 0,
+                          borderData: FlBorderData(
+                            show: false,
+                          ),
+                          gridData: FlGridData(
+                            show: false,
+                          ),
+                          titlesData: FlTitlesData(
+                            show: true,
+                            rightTitles: SideTitles(
+                              showTitles: false,
+                            ),
+                            topTitles: SideTitles(
+                              showTitles: false,
+                            ),
+                            bottomTitles: SideTitles(
+                              showTitles: true,
+                              getTitles: (double value) {
+                                debugPrint(value.toInt().toString());
+                                return value.toInt().toString();
+                                // return widget
+                                //     .testData![(value - 1).toInt()].testname!;
+                              },
+                              getTextStyles: (BuildContext context, value) =>
+                              const TextStyle(
+                                color: Color(0xFF67727D),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12,
+                              ),
+                              margin: 8,
+                              interval: 1,
+                            ),
+                          ),
+                          lineBarsData: [
+                            LineChartBarData(
+                              spots: graphData,
+                              isCurved: true,
+                              colors: [Colors.blue, Colors.green],
+                              barWidth: 2,
+                              isStrokeCapRound: true,
+                              dotData: FlDotData(
+                                show: true,
+                              ),
+                              belowBarData: BarAreaData(
+                                show: true,
+                                colors: [
+                                  Colors.blue.withOpacity(0.3),
+                                  Colors.green.withOpacity(0.3),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+
         FutureBuilder(
           future: tests,
           builder: (context, snapshot) {
@@ -109,6 +300,7 @@ class _AllTabPageState extends State<AllTabPage> {
                   );
                 else if (snapshot.data != null) {
                   List<TestTaken> testData = snapshot.data! as List<TestTaken>;
+                  // List<TestTaken> testTakenData = testData;
                   inspect(testData);
                   return Expanded(
                     child: testData.length > 0
