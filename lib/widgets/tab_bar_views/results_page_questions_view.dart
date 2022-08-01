e39e7c9f6fb7f52df4d77d6d1c71d315042862f3
Sylensa <sylensa.adolf@gmail.com>
2022-08-01 19:42:27 +0000

    minor

--- lib/widgets/tab_bar_views/results_page_questions_view.dart
+++ lib/widgets/tab_bar_views/results_page_questions_view.dart
@@ -670,13 +670,14 @@ List<Question>reviewQuestions = [];
                   child: Button(
                     label: 'new test',
                     onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) {
-                            return TestTypeView(
-                              widget.user,
-                              widget.course!,
-                            );
-                          }));
+                      Navigator.pop(context);
+                      // Navigator.push(context,
+                      //     MaterialPageRoute(builder: (context) {
+                      //       return TestTypeView(
+                      //         widget.user,
+                      //         widget.course!,
+                      //       );
+                      //     }));
                     },
                   ),
                 ),
