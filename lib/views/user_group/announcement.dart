a886a91386f822f88b9ba5bc2c6a307a837315d0
Sylensa <sylensa.adolf@gmail.com>
2022-08-17 23:41:17 +0000

    user group announcement

---
+++ lib/views/user_group/announcement.dart
@@ -0,0 +1,110 @@
+import 'package:ecoach/helper/helper.dart';
+import 'package:ecoach/models/user.dart';
+import 'package:ecoach/revamp/core/utils/app_colors.dart';
+import 'package:ecoach/views/user_group/activities/group_activity.dart';
+import 'package:ecoach/views/user_group/group_activity.dart';
+import 'package:ecoach/widgets/adeo_tab_control.dart';
+import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+
+class Announcement extends StatefulWidget {
+  static const String routeName = '/user_group';
+  Announcement(this.user, {Key? key}) : super(key: key);
+  User user;
+  @override
+  State<Announcement> createState() => _AnnouncementState();
+}
+
+class _AnnouncementState extends State<Announcement> {
+  TextEditingController searchController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Color(0XFFDBF1A8),
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only( bottom: 2.h, left: 2.h, right: 2.h),
+              decoration: BoxDecoration(
+                color: Color(0XFFF4E3E3),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(top: 4.h,),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        GestureDetector(
+                          onTap: (){
+                            Navigator.pop(context);
+                          },
+                          child: Icon(Icons.arrow_back,color: Colors.black,),
+                        ),
+                        SizedBox(width: 20,),
+                        sText("Announcement",weight: FontWeight.bold,size: 20),
+
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 10,),
+                  Row(
+                    children: [
+                      Container(
+                        width: 50,
+                        height: 5,
+                        color: Color(0XFFF7B06E),
+                      ),
+                      SizedBox(width: 10,),
+                      sText("3 days ago",weight: FontWeight.w500,color: Colors.black),
+
+                    ],
+                  ),
+                  SizedBox(height: 10,),
+                  sText("Track all activities",weight: FontWeight.w500,size: 20,color: Colors.black),
+                  SizedBox(height: 5,),
+                  sText("Never miss deadlines",weight: FontWeight.w500,size: 20,color: Colors.black),
+
+                ],
+              ),
+            ),
+
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20,vertical: 20),
+                width: appWidth(context),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(20),topRight: Radius.circular(20))
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+
+                      children: [
+                        sText("Instruction",weight: FontWeight.bold,size: 16,align: TextAlign.left),
+                        SizedBox(height: 20,),
+                        sText("This test is your final assessment before your actual exam. Ensure to answer every question. Any wrongly answered question attracts a mark of -1 Any unanswered question attracts a mark of -1 The test comprises of 30 questions.Your pass mark for the test is 60%. Good luck candidates."),
+
+                      ],
+                    ),
+                   Image.asset("assets/images/announcement.png")
+                  ],
+                ),
+              ),
+            ),
+
+
+          ],
+        ),
+
+      ),
+
+    );
+  }
+}
