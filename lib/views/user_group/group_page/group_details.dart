0b3a02e626cf342701c54fab84d9675dbcf5aecf
Sylensa <sylensa.adolf@gmail.com>
2022-09-07 17:57:46 +0000

    worked on group details page

--- lib/views/user_group/group_page/group_details.dart
+++ lib/views/user_group/group_page/group_details.dart
@@ -1,22 +1,197 @@
+import 'package:ecoach/controllers/group_management_controller.dart';
 import 'package:ecoach/helper/helper.dart';
+import 'package:ecoach/models/group_list_model.dart';
 import 'package:ecoach/models/user.dart';
 import 'package:ecoach/revamp/core/utils/app_colors.dart';
+import 'package:ecoach/utils/app_url.dart';
+import 'package:ecoach/utils/style_sheet.dart';
+import 'package:ecoach/widgets/toast.dart';
+import 'package:ecoach/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class GroupDetails extends StatefulWidget {
   static const String routeName = '/user_group';
-  GroupDetails(this.user, {Key? key}) : super(key: key);
-  User user;
+  GroupListData? groupData;
+  GroupDetails( {Key? key,this.groupData,}) : super(key: key);
   @override
   State<GroupDetails> createState() => _GroupDetailsState();
 }
 
 class _GroupDetailsState extends State<GroupDetails> {
   TextEditingController searchController = TextEditingController();
+  getGroupDetails() async {
+    final bool isConnected = await InternetConnectionChecker().hasConnection;
+    // try {
+    if (isConnected) {
+      widget.groupData = await GroupManagementController(groupId: widget.groupData!.id.toString()).getGroupDetails();
+    } else {
+      showNoConnectionToast(context);
+    }
+    // } catch (e) {
+    //   print(e.toString());
+    // }
+
+    setState(() {
+    });
+  }
+  late String generatedLink = '';
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  authorisePayment(BuildContext context, int groupId) async {
+    var res = await doPost(AppUrl.groupSubscription, {
+      "group_id": groupId,
+    });
+    print("res:$res");
+    if (res["status"]) {
+      Navigator.pop(_scaffoldKey.currentContext!);
+      generatedLink = res["data"]["authorization_url"];
+      paymentPage(generatedLink);
+    } else {
+      Navigator.pop(_scaffoldKey.currentContext!);
+      toastMessage(res["message"]);
+    }
+  }
+
+  paymentPage(String authorizationUrl) {
+    showDialog(
+      context: _scaffoldKey.currentContext!,
+      builder: (context) {
+        return WebView(
+          javascriptMode: JavascriptMode.unrestricted,
+          initialUrl: authorizationUrl,
+          navigationDelegate: (navigation) async {
+            //Listen for callback URL
+            if (navigation.url.contains('https://standard.paystack.co/close')) {
+              print("hey:");
+              Navigator.of(context).pop(); //close webview
+            }
+            if (navigation.url.contains(AppUrl.payment_callback)) {
+              Navigator.of(context).pop();
+
+              setState(() {
+                print("hello adolf:");
+              });
+            }
+            return NavigationDecision.navigate;
+          },
+        );
+      },
+    );
+  }
+
+  upgradePackageModalBottomSheet(context,) {
+    double sheetHeight = 300;
+    showModalBottomSheet(
+        context: context,
+        isDismissible: true,
+        backgroundColor: Colors.transparent,
+        isScrollControlled: true,
+        builder: (BuildContext context) {
+          return StatefulBuilder(
+            builder: (BuildContext context, StateSetter stateSetter) {
+              return Container(
+                  height: sheetHeight,
+                  decoration: BoxDecoration(
+                      color: kAdeoGray,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30),
+                      )),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        color: Colors.grey,
+                        height: 5,
+                        width: 100,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: Center(
+                            child: sText("${widget.groupData!.description}",
+                                weight: FontWeight.bold,
+                                size: 20,
+                                align: TextAlign.center)),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: sText("Join Group",
+                            color: kAdeoGray3,
+                            weight: FontWeight.w400,
+                            align: TextAlign.center),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 20),
+                        margin: EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 10),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius:
+                            BorderRadius.circular(10)),
+                        child: Row(
+                          mainAxisAlignment:
+                          MainAxisAlignment.spaceBetween,
+                          children: [
+                            sText(widget.groupData!.name,
+                                color: kAdeoGray3),
+                            sText(
+                                widget.groupData!.settings != null ?  "${widget.groupData!.settings!.settings!.currency} ${widget.groupData!.settings!.settings!.amount}" : "Free",
+                                color: Colors.black,
+                                weight: FontWeight.bold),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      MaterialButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                    showLoaderDialog(context);
+                    authorisePayment(context, widget.groupData!.id!);
+                  },
+                  child: Container(
+                    width: appWidth(context),
+                    padding: EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 20),
+                    margin: EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10),
+                    decoration: BoxDecoration(
+                        color: Colors.orange,
+                        borderRadius:
+                        BorderRadius.circular(10)),
+                    child: sText("Join Now",color: Colors.white,weight: FontWeight.bold,align: TextAlign.center),
+                  ),
+                ),
+                    ],
+                  ));
+            },
+          );
+        });
+  }
+  @override
+ void initState(){
+    super.initState();
+    // getGroupDetails();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       backgroundColor: Colors.white,
       body: Container(
         // padding: EdgeInsets.only(top: 2.h, bottom: 2.h, left: 2.h, right: 2.h),
@@ -44,7 +219,7 @@ class _GroupDetailsState extends State<GroupDetails> {
                        ),
                         GestureDetector(
                           onTap: ()async{
-
+                            upgradePackageModalBottomSheet(_scaffoldKey.currentContext);
                           },
                           child: Container(
                             padding: EdgeInsets.symmetric(vertical: 8,horizontal: 8),
