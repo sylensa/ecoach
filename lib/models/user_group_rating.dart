bcbed0cc17c314ad1ef4cd5347f8a29cd3235894
Sylensa <sylensa.adolf@gmail.com>
2022-09-09 21:48:12 +0000

    user object added to review

--- lib/models/user_group_rating.dart
+++ lib/models/user_group_rating.dart
@@ -45,6 +45,7 @@ class GroupRatingData {
     this.updatedAt,
     this.createdAt,
     this.id,
+   this.member
   });
 
   String? groupId;
@@ -54,6 +55,7 @@ class GroupRatingData {
   DateTime? updatedAt;
   DateTime? createdAt;
   int? id;
+  Member? member;
 
   factory GroupRatingData.fromJson(Map<String, dynamic> json) => GroupRatingData(
     groupId: json["group_id"] == null ? null : json["group_id"],
@@ -63,6 +65,8 @@ class GroupRatingData {
     updatedAt: json["updated_at"] == null ? null : DateTime.parse(json["updated_at"]),
     createdAt: json["created_at"] == null ? null : DateTime.parse(json["created_at"]),
     id: json["id"] == null ? null : json["id"],
+    member: json["member"] == null ? null : Member.fromJson(json["member"]),
+
   );
 
   Map<String, dynamic> toJson() => {
@@ -73,5 +77,43 @@ class GroupRatingData {
     "updated_at": updatedAt == null ? null : updatedAt!.toIso8601String(),
     "created_at": createdAt == null ? null : createdAt!.toIso8601String(),
     "id": id == null ? null : id,
+    "member": member == null ? null : member!.toJson(),
+
+  };
+}
+
+class Member {
+  Member({
+    this.id,
+    this.name,
+    this.email,
+    this.phone,
+    this.isAgent,
+    this.isGroupCreator,
+  });
+
+  int? id;
+  String? name;
+  String? email;
+  String? phone;
+  bool? isAgent;
+  bool? isGroupCreator;
+
+  factory Member.fromJson(Map<String, dynamic> json) => Member(
+    id: json["id"] == null ? null : json["id"],
+    name: json["name"] == null ? null : json["name"],
+    email: json["email"] == null ? null : json["email"],
+    phone: json["phone"] == null ? null : json["phone"],
+    isAgent: json["is_agent"] == null ? null : json["is_agent"],
+    isGroupCreator: json["is_group_creator"] == null ? null : json["is_group_creator"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id == null ? null : id,
+    "name": name == null ? null : name,
+    "email": email == null ? null : email,
+    "phone": phone == null ? null : phone,
+    "is_agent": isAgent == null ? null : isAgent,
+    "is_group_creator": isGroupCreator == null ? null : isGroupCreator,
   };
 }
