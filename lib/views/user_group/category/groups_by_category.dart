0339fe4417ae56f9175c64ec6b59f43b5ab85767
Sylensa <sylensa.adolf@gmail.com>
2022-09-09 21:46:52 +0000

    bug fix

--- lib/views/user_group/category/groups_by_category.dart
+++ lib/views/user_group/category/groups_by_category.dart
@@ -18,13 +18,13 @@ class CategoryGroupsPage extends StatefulWidget {
 }
 
 class _CategoryGroupsPageState extends State<CategoryGroupsPage> {
-  List<GroupListData> groupBySearch= [];
-  bool progressCodeSearch = false;
+  List<GroupListData> groupByCategory= [];
+  bool progressCodeSearch = true;
   searchGroupByCategory() async {
     final bool isConnected = await InternetConnectionChecker().hasConnection;
     try {
       if (isConnected) {
-        groupBySearch = await GroupManagementController().searchGroupList(category: properCase(widget.categoryName));
+        groupByCategory = await GroupManagementController().getGroupsByCategory(category: properCase(widget.categoryName));
       } else {
         showNoConnectionToast(context);
       }
@@ -33,7 +33,7 @@ class _CategoryGroupsPageState extends State<CategoryGroupsPage> {
     }
 
     setState(() {
-      progressCodeSearch = true;
+      progressCodeSearch = false;
     });
   }
   @override
@@ -51,119 +51,25 @@ class _CategoryGroupsPageState extends State<CategoryGroupsPage> {
           Navigator.pop(context);
         }, icon: Icon(Icons.arrow_back_ios,color: Colors.black,)),
         elevation: 0,
-        title: sText("Category Name",weight: FontWeight.bold),
+        title: sText("${properCase(widget.categoryName)}",weight: FontWeight.bold),
       ),
       body: Container(
         child: Column(
           children: [
+            groupByCategory.isNotEmpty ?
             Expanded(
               child: ListView.builder(
-                  itemCount: 10,
+                  itemCount: groupByCategory.length,
                   itemBuilder: (BuildContext context, int index){
                     return  MaterialButton(
                       padding: EdgeInsets.zero,
                       onPressed: (){
-                        goTo(context, GroupDetails());
+                        goTo(context, GroupDetails(groupData: groupByCategory[index],));
                       },
-                      child: Container(
-                        padding: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
-                        margin: EdgeInsets.symmetric(horizontal: 20,vertical: 5),
-                        decoration: BoxDecoration(
-                            color: Color(0XFFF5F5F5),
-                            borderRadius: BorderRadius.circular(10)
-                        ),
-                        child: Row(
-                          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Container(
-                              child: Image.asset("assets/images/fCvBipe.png"),
-                            ),
-                            SizedBox(width: 5,),
-                            Container(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Container(
-                                    child: Row(
-                                      children: [
-                                        Container(
-                                          child: sText("Adeo Group",size: 14,weight: FontWeight.bold),
-                                        ),
-                                        SizedBox(width: 5,),
-                                        Container(
-                                          child: sText("by Afram Dzidefo",size: 10,weight: FontWeight.normal,color: Colors.grey),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                  Row(
-                                    children: [
-                                      Container(
-                                        child: Row(
-                                          children: [
-                                            Container(
-                                              child: Icon(Icons.groups,color: Colors.grey,size: 14,),
-                                            ),
-                                            SizedBox(width: 5,),
-                                            Container(
-                                              child: sText("1,200",size: 10,weight: FontWeight.normal,),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                      SizedBox(width: 5,),
-                                      Container(
-                                        child: Row(
-                                          children: [
-                                            Container(
-                                              child: Icon(Icons.star,color: Colors.orange,size: 14,),
-                                            ),
-                                            SizedBox(width: 0,),
-                                            Container(
-                                              child: sText("4.0",size: 10,weight: FontWeight.w400,),
-                                            ),
-                                            SizedBox(width: 5,),
-                                            Container(
-                                              child: sText("(200 reviews)",size: 10,weight: FontWeight.normal,),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                      SizedBox(width: 5,),
-                                      Container(
-                                        child: Row(
-                                          children: [
-                                            Container(
-                                              child: Image.asset("assets/images/label.png"),
-                                            ),
-                                            SizedBox(width: 5,),
-                                            Container(
-                                              child: sText("GHS 99",size: 10,weight: FontWeight.bold,),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-
-                                ],
-                              ),
-                            ),
-                            Expanded(child: Container()),
-                            Container(
-                              padding: EdgeInsets.symmetric(vertical: 8,horizontal: 8),
-                              child: sText("JOIN",color: Colors.white,weight: FontWeight.bold),
-                              decoration: BoxDecoration(
-                                  color: Colors.orange,
-                                  borderRadius: BorderRadius.circular(10)
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
+                      child: groupListWidget(groupByCategory[index])
                     );
                   }),
-            )
+            ) : progressCodeSearch ? Expanded(child: Center(child: progress(),)) : Expanded(child: Center(child:sText("No groups for this category"),))
           ],
         ),
       ),
